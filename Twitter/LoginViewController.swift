7b52fe0bce46c0af2e8d83e02a5c301ffde24429
Kim Pham <ktp011@ucsd.edu>
2019-12-21 02:06:05 -0800

    implemented staying logged in, polish tweet cells with user's name, user's tweet, and user's profile image

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -16,10 +16,21 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    //keep user logged in
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDefaults.standard.bool(forKey: "userLoggedIn") == true {
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }
+    }
 
+    //when user press login button
     @IBAction func onLoginButton(_ sender: Any) {
-        let url = "https://api.twitter.com/oauth/request_token"
-        TwitterAPICaller.client?.login(url: url, success: {
+        let tokenUrl = "https://api.twitter.com/oauth/request_token"
+        TwitterAPICaller.client?.login(url: tokenUrl, success: {
+            
+            //keep user logged in
+            UserDefaults.standard.set(true, forKey: "userLoggedIn")
+            
             self.performSegue(withIdentifier: "loginToHome", sender: self)
         }, failure: { (Error) in
             print ("Could not login")
